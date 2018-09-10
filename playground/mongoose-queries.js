9b5a6bcd0ec6c210e56d9b5258a7bcf6d42c5b57
Rodrigo Martín de Lamo <mcgod69esp@gmail.com>
2018-09-10 16:03:51 +0200

    Add get /todos/:id

---
+++ playground/mongoose-queries.js
@@ -0,0 +1,30 @@
+const {ObjectID} = require('mongodb');
+
+const { mongoose } = require('./../server/db/mongoose');
+const { Todo } = require('./../server/models/todo');
+
+var id = '5b96742d9fd07f08493d1c80';
+
+if (!ObjectID.isValid(id)) {
+    console.log('ID not valid');
+}
+
+/* 
+Todo.find({
+    _id: id
+}).then((todos) => {
+    console.log('Todos', todos);
+});
+
+Todo.findOne({
+    _id: id
+}).then((todo) => {
+    console.log('Todo', todo);
+});
+ */
+Todo.findById(id).then((todo) => {
+    if (!todo) {
+        return console.log('Id not found');
+    }
+    console.log('Todo by id', todo);
+}).catch((e) => console.log(e));
--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var { ObjectID } = require('mongodb');
 
 var { mongoose } = require('./db/mongoose');
 var { Todo } = require('./models/todo');
@@ -39,6 +40,23 @@ app.post('/user', (req, res) => {
     }).catch((e) => {
         res.status(400).send(e);
     });
+});
+
+app.get('/todos/:id', (req, res) => {
+    var id = req.params.id;
+    if(!ObjectID.isValid(id)) {
+        res.status(404).send();
+    } else {
+        Todo.findById(id).then((todo) => {
+            if(todo) {
+                res.send(todo);
+            } else {
+                res.status(404).send();
+            }
+        }).catch((e) => {
+            res.status(400).send();
+        })
+    }
 });
 
 app.listen(3000, () => {
