3129771dd6e913d5065b0c9dda321430fcb7211b
Rodrigo Martín de Lamo <mcgod69esp@gmail.com>
2018-09-10 16:07:50 +0200

    missing changes

--- server/server.js
+++ server/server.js
@@ -44,19 +44,19 @@ app.post('/user', (req, res) => {
 
 app.get('/todos/:id', (req, res) => {
     var id = req.params.id;
-    if(!ObjectID.isValid(id)) {
-        res.status(404).send();
-    } else {
-        Todo.findById(id).then((todo) => {
-            if(todo) {
-                res.send(todo);
-            } else {
-                res.status(404).send();
-            }
-        }).catch((e) => {
-            res.status(400).send();
-        })
+    if (!ObjectID.isValid(id)) {
+        return res.status(404).send();
     }
+    Todo.findById(id).then((todo) => {
+        if (todo) {
+            res.send(todo);
+        } else {
+            res.status(404).send();
+        }
+    }).catch((e) => {
+        res.status(400).send();
+    })
+
 });
 
 app.listen(3000, () => {
