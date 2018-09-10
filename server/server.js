94bb75238e997a8b9dc83c0cc3aaeb38b2502d6b
Rodrigo Martín de Lamo <mcgod69esp@gmail.com>
2018-09-10 15:26:29 +0200

    Test POST /todos route

--- server/server.js
+++ server/server.js
@@ -21,6 +21,20 @@ app.post('/todos', (req, res) => {
     });
 })
 
+app.post('/user', (req, res) => {
+    var user = new User({
+        email: req.body.email
+    });
+
+    user.save().then((doc) => {
+        res.send(doc);
+    }).catch((e) => {
+        res.status(400).send(e);
+    });
+});
+
 app.listen(3000, () => {
     console.log('Started on port 3000');
 })
+
+module.exports = { app };
