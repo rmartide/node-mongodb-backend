211b1ca99ec66423703fbf3d40ee766e15d08f4c
Rodrigo Martín de Lamo <mcgod69esp@gmail.com>
2018-09-12 08:42:03 +0200

    Add GET /users/me and authentication middleware

--- server/models/user.js
+++ server/models/user.js
@@ -45,11 +45,28 @@ UserSchema.methods.generateAuthToken = function () {
     var token = jwt.sign({ _id: user._id.toHexString(), access }, 'abc123').toString();
 
     user.tokens = user.tokens.concat([{ access, token }]);
-    
+
     return user.save().then(() => {
         return token;
     });
 };
+
+UserSchema.statics.findByToken = function (token) {
+    var User = this;
+    var decoded;
+
+    try {
+        decoded = jwt.verify(token, 'abc123');
+    } catch (e) {
+        return Promise.reject();
+    }
+
+    return User.findOne({
+        '_id': decoded._id,
+        'tokens.token': token,
+        'tokens.access': 'auth'
+    });
+}
 
 var User = mongoose.model('User', UserSchema);
 
