744bc942e383b9a8f8068ff24de7c39cd0d6c8a6
Rodrigo Martín de Lamo <mcgod69esp@gmail.com>
2018-09-12 09:08:43 +0200

    Hash user password before saving

--- playground/hashing.js
+++ playground/hashing.js
@@ -1,7 +1,22 @@
 const { SHA256 } = require('crypto-js');
 const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
 
-var data = {
+var password = '123abc!';
+
+bcrypt.genSalt(10, (err, salt) => {
+    bcrypt.hash(password, salt, (err, hash) => {
+        console.log(hash);
+    });
+});
+
+var hashedPassword = '$2a$10$2IGX4GS2d1dCtaOfSl5wzuFbvjpoEVYR8B4Sv2oSbqSq0plyoNrUS';
+
+bcrypt.compare(password, hashedPassword, (err, res) => {
+    console.log(res);
+});
+
+/* var data = {
     id: 10
 };
 
@@ -9,7 +24,7 @@ var token = jwt.sign(data, '123abc');
 console.log(token);
 
 var decoded = jwt.verify(token, '123abc');
-console.log(decoded);
+console.log(decoded); */
 
 
 /* var message = 'I am user number 3';
