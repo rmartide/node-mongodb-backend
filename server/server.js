d1df9c601090e90450a72210539619282587ee60
Rodrigo Martín de Lamo <mcgod69esp@gmail.com>
2018-09-13 08:16:12 +0200

    Add POST /users/login

--- server/server.js
+++ server/server.js
@@ -109,11 +109,23 @@ app.post('/users', (req, res) => {
     }).catch((err) => {
         res.status(400).send(err);
     });
-})
+});
 
 app.get('/users/me', authenticate, (req, res) => {
     res.send(req.user);
-})
+});
+
+app.post('/users/login', (req, res) => {
+    var body = _.pick(req.body, ['email', 'password']);
+
+    User.findByCredentials(body.email, body.password).then((user) => {
+        return user.generateAuthToken().then((token) => {
+            res.header('x-auth', token).send(user);
+        });
+    }).catch((e) => {
+        res.status(400).send();
+    });
+});
 
 app.listen(port, () => {
     console.log(`Started on port ${port}`);
