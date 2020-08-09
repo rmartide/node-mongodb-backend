98d278bd955cd85070b1629f4cf5ba1c5ab5aadf
Rodrigo Martín de Lamo <mcgod69esp@gmail.com>
2020-08-09 22:51:18 +0200

    Updated dependencies and code to use these

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -2,6 +2,7 @@ var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
 console.log(process.env.MONGODB_URI)
-mongoose.connect(process.env.MONGODB_URI);
+mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.set('useCreateIndex', true);
 
 module.exports = { mongoose };
